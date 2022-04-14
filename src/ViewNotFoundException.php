05919fa4c0ff23f74b1718aa530c6e290c81c042
isszz <10218431@qq.com>
2022-04-14 14:13:34 +0800

    Update ViewNotFoundException.php

--- src/ViewNotFoundException.php
+++ src/ViewNotFoundException.php
@@ -6,19 +6,30 @@ class ViewNotFoundException extends \RuntimeException
 {
     protected $template;
 
-    public function __construct(string $message, string $template = '')
+    public function __construct(string $message, string $template = '', string $controller = '')
     {
         $this->message  = $message;
         $this->template = $template;
+        $this->controller = $controller;
     }
 
     /**
-     * 获取模板文件
-     * @access public
+     * 获取报错模板文件|非路劲
+     *
      * @return string
      */
     public function getTemplate(): string
     {
         return $this->template;
+    }
+
+    /**
+     * 获取报错模板所属控制器
+     *
+     * @return string
+     */
+    public function getController(): string
+    {
+        return $this->controller;
     }
 }
